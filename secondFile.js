d22f449d6bbdbf0d3f50d0700d9093a4954d054c
Lohan000 <lohannunes07@gmail.com>
2026-03-25 21:48:43 -0300

    Add files via upload

---
+++ secondFile.js
@@ -0,0 +1,23 @@
+/*const got = "Game of Thrones"
+const GotSeasons = 8
+const iLoveGot = false
+
+console.log(typeof iLoveGot)
+
+let type = typeof iLoveGot;
+console.log(type)
+
+const idadeNumero = 23
+const idadeTexto = idadeNumero.toString()
+
+console.log(typeof idadeNumero)
+console.log(typeof idadeTexto)
+*/
+
+//Exercício 1
+let Age = 16
+//Exercicio 2
+let MyAge = Age.toString();
+//Exercicio 3
+console.log(typeof Age)
+console.log(MyAge, typeof MyAge)
