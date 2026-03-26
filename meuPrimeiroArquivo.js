7e5cc65f8a113f20a699ed104971c9a116ee38cc
Lohan000 <lohannunes07@gmail.com>
2026-03-25 21:49:32 -0300

    Add files via upload

---
+++ meuPrimeiroArquivo.js
@@ -0,0 +1,27 @@
+/*let novaVariavel = 100;
+console.log(novaVariavel);
+
+novaVariavel = "Leonardo"
+console.log(novaVariavel)
+
+const nome = "Lohan";
+const idade = "33";
+console.log("Olá, seu nome é", nome,"e sua idade é:",idade,  "sou um gênio, ne?")
+
+let minhaVariavel = "Leonardo 25"
+console.log(minhaVariavel)
+*/
+//Exercicio 1
+let primeiroNome = ("Lohan")
+
+//Exercicio 2
+let segundoNome = ("Nunes")
+
+//Exercicio 3
+let idade = ("16")
+
+//Exercicio 4
+let estudante = true
+
+//Exercicio 5
+console.log("Meu nome eh:", primeiroNome, segundoNome,"Tenho:", idade,"anos", "Estudante?", estudante)
