684ccbc469751cacaa7cddfe47176b668a9042ac
Lohan000 <lohannunes07@gmail.com>
2026-03-25 21:36:21 -0300

    Add files via upload

---
+++ threeFile.js
@@ -0,0 +1,27 @@
+/*
+Exercicio 1
+let num1 = 3
+num1 += 4
+
+console.log(num1)
+
+Exercicio 2
+let num1 = 3
+num1 *= 5
+num1 /= 2
+console.log(num1)
+
+Exercicio 3
+
+let num1 = 4
+num1 -= 5
+num1 *= -1
+console.log(num1)
+
+/*
+
+Exercicio 4
+let num1 = 234
+num1 %= 5
+console.log(num1)
+*/
