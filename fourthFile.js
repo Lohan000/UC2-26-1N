cb2847a420805f593b9a2806e8608975b9c9b45a
Lohan000 <lohannunes07@gmail.com>
2026-03-25 21:45:42 -0300

    Add files via upload

---
+++ fourthFile.js
@@ -0,0 +1,7 @@
+let num1 = 2
+let num2 = 2
+
+console.log("1.O primeiro número é igual ao segundo?", num1 === num2)
+console.log("2.o primeiro número é diferente do segundo?", num1 !== num2)
+console.log("3.O primeiro número é maior que o segundo?", num1 > num2)
+console.log("4.O primeiro número é menor que o segundo?", num1 < num2)
