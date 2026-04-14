4d1c62d053cc55eb23eec82c039e4e7d0e0a496a
Lohan000 <lohannunes07@gmail.com>
2026-04-13 21:46:13 -0300

    Add files via upload

---
+++ eightFile.js
@@ -0,0 +1,39 @@
+/*
+const ask = require('readline-sync');
+
+let nome = ask.question("Qual é o seu nome?");
+console.log("Olá,", nome);
+
+let name = "Lohan"
+let sobrenome = "Nunes Rodrigues"
+
+console.log(`Meu nome é ${name} sobrenome e tenho ${sobrenome} anos`);
+
+const ask = require('readline-sync')
+
+let name = ask.question("Qual é o seu nome?");
+let colour = ask.question("Qual é sua cor favorita?");
+
+console.log(`seu nome eh ${name} e sua cor favorita... ${colour}`);
+
+
+let name = "Lohan Nunes"
+console.log(name.length);
+
+
+let frase = "OieEeEee!"
+let fraseMinuscula = frase.toLowerCase()
+console.log(fraseMinuscula)
+
+let frase = "Hoje comi cenoura"
+let frase1 = frase.includes("cenoura")
+let frase2 = frase.includes("pão")
+
+console.log(`${frase1}, ${frase2}`);
+
+const ask = require(readline-sync)
+
+let aaa = "Hoje comi cenoura, adoro cenoura"
+let novaFrase = frase.replaceAll("cenoura", "batata")
+console.log(`${frase}`);
+*/
