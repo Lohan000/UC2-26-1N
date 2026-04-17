89ee606cc775f23da6497550fd60398fe581f50d
Lohan000 <lohannunes07@gmail.com>
2026-04-16 21:48:25 -0300

    Add files via upload

---
+++ fourteenFile.js
@@ -0,0 +1,30 @@
+const ask = require('readline-sync')
+
+//2
+
+/*
+const opiniao = ask.question("Digite sua opiniao sobre o acidente: ")
+const opiniaoMaisculua = opiniao.toUpperCase()
+console.log(`Opiniao original: ${opiniao} \nOpiniao maiscula: ${opiniaoMaisculua}`)
+*/
+
+//3
+
+/*
+let mensagem3 = "    trem parado por causa de um caminhão nos trilhos     "
+let semEspaco = mensagem3.trim();
+console.log(`mensagem original ${mensagem3} \n mensagem 
+ajustada: ${semEspaco}`);
+console.log(`${mensagem3.length} \n ${semEspaco.length}`);
+*/
+
+//4
+
+/*
+let frase = ask.question("Digite uma frase sobre o ocorrido: ");
+let palavra = ask.question("Digite uma palavra a ser verificada: ");
+console.log(`Frase: ${frase}
+Palavra: ${palavra}
+${palavra} aparece em ${frase}?
+ ${frase.includes(palavra)}`);
+ */
