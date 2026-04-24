6055a94d46c11819499bb1b2acc8ba1bde38ce6a
Lohan000 <lohannunes07@gmail.com>
2026-04-23 21:51:00 -0300

    Add files via upload

---
+++ eighteenFile.js
@@ -0,0 +1,60 @@
+const ask = ("readline-sync")
+
+/*
+function imprimirOlaMundo() {
+    console.log("Olá Mundo!")
+}
+imprimirOlaMundo()
+imprimirOlaMundo()
+imprimirOlaMundo()
+*/
+/*
+function calculaArea(altura, largura){
+    const area = altura * largura
+    console.log(area)
+}
+
+calculaArea(6,5)
+*/
+
+// Exercício 2
+// function print(nome){
+    // console.log(`Ola ${nome}`)
+// }
+// print("Lohan")
+// print("Kauã")
+// print("Igor")
+
+// function calculaArea(altura, largura){
+    // const area = altura * largura
+    // return area
+// }
+
+// console.log(calculaArea(5,8))
+
+// let areaCalculada = calculaArea(7, 9)
+
+// console.log(calculaArea(7, 12 > 100))
+/*
+function plus(number1, number2){
+    soma = number1 + number2
+    return soma
+}
+
+let firstNumber = Number(ask.question("digite o primeiro Número"))
+let segundoNumero = Number(ask.question("digite o segundo Número"))
+
+console.log(`${plus(firstNumber, segundoNumero)}`)
+*/
+/*
+let variavelArray = [1, 2, 3, 4, 5, 6, 9]
+
+function numberExe(array){
+    let arrayRetorno = []
+    arrayRetorno.push((array[array.length - 1]/2))
+    arrayRetorno.push((array[0])/2)
+
+}
+
+console.log(exercicio4(variavelarray))
+*/
