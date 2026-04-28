362d41db7cd12390a268d3d211dbfa0c359f424b
Lohan000 <lohannunes07@gmail.com>
2026-04-27 21:49:57 -0300

    Add files via upload

---
+++ avaliationFile.js
@@ -0,0 +1,27 @@
+/*
+const ask = require("readline-sync")
+
+nome = ask.question("Qual o seu nome:")
+idade = ask.question("Qual sua idade:")
+cidade = ask.question("Qual sua cidade; ")
+hobbies = ask.question("Qual são os seus hobbies: ")
+
+dados = [nome, idade, cidade, hobbies]
+*/
+function guy(nome, idade, cidade, hobbies){
+    dados = [nome, idade,  cidade, hobbies]
+    function formatarHobbies(){
+        hobbies = ["música", "jogos", "futebol"]
+        function gerarDescricao(){
+            let user = criarUsario('')
+            usuario = ["Lohan", "16", "São Leopoldo", "Jogos"]
+
+        }
+    }
+    return dados
+}
+
+// console.log(guy('Lohan', 16, 'São leo', 'Jogos'))
+
+
+
