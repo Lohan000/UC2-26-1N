3c6ce92d46145ef241e07c93015852955586fa41
Lohan000 <lohannunes07@gmail.com>
2026-05-18 21:54:24 -0300

    Add files via upload

---
+++ challengeFile.js
@@ -0,0 +1,85 @@
+const ask = require('readline-sync')
+// let registerusers = []
+
+// function cadastrarAluno(){
+//         user = [aluno, notas, media, situacao]
+//         console.log(`Aluno: ${aluno} adicionado`)
+//         registerusers.push(user)
+//     return    
+// }
+
+// function calcularMedia(nota1, nota2, nota3){
+//     resultadonota = (nota1 + nota2 + nota3) /3
+//     console.log(resultadonota)
+//     return resultadonota
+// }
+
+// function verificarSituacao(result){
+//     if (result => 7){
+//     return "Aprovado"
+
+//    }  else if ((result >= 5) && (result< 7)){
+//     return "Recuperação"
+
+//    } else {
+//     return "Reprovado"
+//    }
+   
+// }
+// function mostrarAlunos(array){
+//     console.log(array)
+// }
+
+// function sistema(number){
+//     switch (number) {
+//         case '1':
+//             console.clear()
+//             let name = ask.question("Qual é o seu nome? ")
+//             let note1 = Number(ask.question("Qual é a sua primeira nota? "))
+//             let note2 = Number(ask.question("Qual é a sua segunda nota? "))
+//             let note3 = Number(ask.question("Qual é a sua terceira nota? "))
+//             let notasgerais = `${note1}, ${note2}, ${note3}`
+//             console.log(notasgerais)
+//             let media = calcularMedia(note1, note2, note3)
+//             console.log(media)
+//             let situaçao = verificarSituacao(media)
+//             cadastrarAluno(name,notasgerais,media,situaçao )
+//             break
+//             case '2':
+//             mostrarAlunos(regist)
+//             break
+//         }
+// }   
+
+// function main(){
+//     while (true){
+//     show = ask.question(`O que voce quer fazer?
+// 1 - Cadastrar o usuario
+// 2 - Ver os usuarios
+// 3 - encerrar o sistema`)
+//     sistema(show)
+
+// }
+
+// }
+
+// main()
+
+// remake
+
+function registerStudent(){
+    let name = ask.question("Qual é o seu nome? ")
+    let note1 = ask.question(`Sua primeira nota: `)
+    let note2 = ask.question(`Sua segunda nota: `)
+    let note3 = ask.question(`Sua terceira nota: `)
+    register.push([name, note1, note2, note3])
+}
+
+function calculateAverage(){
+    
+}
+
+
+
+
+const register = []
